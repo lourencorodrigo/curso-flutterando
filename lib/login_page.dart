0cb0a38d61f5f2c78e1f07f04b69abd1ff5b8f6a
Rodrigo Lourenço <rsilvape@gmail.com>
2021-10-10 22:53:52 -0300

    aula 23 - Stack

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -13,57 +13,77 @@ class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 150,
-                  height: 150,
-                  child: Image.asset('assets/images/logo.png'),
-                ),
-                Container(height: 16),
-                TextField(
-                  onChanged: (text) {
-                    email = text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Email'),
-                ),
-                SizedBox(height: 10),
-                TextField(
-                  onChanged: (text) {
-                    password = text;
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(), labelText: 'Password'),
-                ),
-                SizedBox(height: 10),
-                ElevatedButton(
-                  onPressed: () {
-                    if (email == 'rsilvape@gmail.com' && password == '123') {
-                      Navigator.of(context).pushReplacementNamed('/home');
-                    } else {
-                      print('ERRADO');
-                    }
-                  },
-                  child: Text('ENTRAR'),
-                )
-              ],
+  Widget _body() {
+    return Column(
+      children: [
+        SingleChildScrollView(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 150,
+                    height: 150,
+                    child: Image.asset('assets/images/logo.png'),
+                  ),
+                  Container(height: 16),
+                  TextField(
+                    onChanged: (text) {
+                      email = text;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Email'),
+                  ),
+                  SizedBox(height: 10),
+                  TextField(
+                    onChanged: (text) {
+                      password = text;
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), labelText: 'Password'),
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (email == 'rsilvape@gmail.com' && password == '123') {
+                        Navigator.of(context).pushReplacementNamed('/home');
+                      } else {
+                        print('ERRADO');
+                      }
+                    },
+                    child: Text('ENTRAR'),
+                  )
+                ],
+              ),
             ),
           ),
         ),
-      ),
+      ],
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'assets/images/background.jpeg',
+              fit: BoxFit.cover,
+            )),
+        Container(
+          color: Colors.black.withOpacity(0.3),
+        ),
+        _body()
+      ],
+    ));
   }
 }
